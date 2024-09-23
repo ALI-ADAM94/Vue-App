af30df4639926738431c9717377773e19017c356
ALI-ADAM94 <aliadam081194@gmail.com>
2024-09-23 20:38:57 +0100

    First Version for Fisrt SFC

---
+++ firstsfc/src/components/FoodItem.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h2>
+            Name :{{ foodName }}
+            <img src="/q.png" v-show="isFavorite">
+        </h2>
+        <p>{{ foodDesc }}</p>
+        <button @click="toggleFavorite">Favorite</button>
+    
+    </div>
+</template>
+
+<script>
+export default{
+
+props: ['foodName','foodDesc','isFavorite'],
+emits:['toggle-favorite'],
+data(){
+    return{
+        foodIsFavorite:this.isFavorite
+    }
+},
+
+methods:{
+toggleFavorite(){
+  //  this.foodIsFavorite= !this.foodIsFavorite;
+    this.$emit('toggle-favorite',this.foodName);
+}  
+}  
+}
+
+
+
+
+
+</script>
+<style>
+img{
+    height: 1.5em;
+    float: right;
+}
+</style>
